299c0323ab14a8b42cc8bd666c6a6be95259c7ec
Tapesh Sharma <tapeshs1134@gmail.com>
2024-08-25 20:10:48 +0530

    add branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
   );  
 }
 
