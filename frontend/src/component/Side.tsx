2853ca0a71949d0c3d8af22283c373d119dbc3dd
dhruvvsh <dhruvsharm02@gmail.com>
2024-08-28 15:09:53 +0530

    fixing

--- frontend/src/component/Side.tsx
+++ frontend/src/component/Side.tsx
@@ -4,7 +4,7 @@ import { FaHome, FaGlobe, FaCog, FaPlus } from "react-icons/fa";
 
 const Side: React.FC = () => {
   return (
-    <aside className="flex flex-col items-center w-20 bg-gray-800 py-4 border-r border-gray-700">
+    <aside className="flex flex-col items-center w-12 h-30 bg-gray-800 py-4 border-r border-gray-700">
       <div className="mb-6">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThsapwuIZ2JPUVRaWSoX_xoEIOHWxneY7EupS8gsFriA&s"
