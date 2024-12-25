e2714eec2615bd863f505eab6a05524d72497634
tapesh1134 <tapeshs1134@gmail.com>
2024-12-25 22:47:27 +0530

    fixing bugs

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-800 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to the Event Management Platform</h1>
+      <h1 className="text-4xl font-bold"></h1>
     </div>
   );
 };
